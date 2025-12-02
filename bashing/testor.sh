372db54e93662991779554f0f23b28d006fc70ee
Pranay <pranay.miyan@proton.me>
2025-12-02 14:43:11 +0000

    rm -r to remove

--- bashing/testor.sh
+++ bashing/testor.sh
@@ -1,3 +1,7 @@
-echo -n "Hi there";echo "World!"
+echo -n "Hi there ";echo "World!"
+echo -e "Hello\nWorld"
 sleep 3
 echo "Pwd for directory, cd - for redo of cd, cd ~ for going in home dir"
+
+# cat is for printing whole file
+
