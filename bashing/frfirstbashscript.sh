b64f253881438b03935c6308ff444b2951d8ca79
Pranay <pranay.miyan@proton.me>
2025-12-10 10:55:06 +0000

    position argument...

---
+++ bashing/frfirstbashscript.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+echo "Hello world!"
+echo "pwd prints current dir, i forgor ts"
+
+#firstname=Pranay
+#secondname=Miyan
+echo "What is your first name"
+read firstname
+echo "What is your second name"
+read secondname
+echo "Hi there, I am $firstname $secondname"
+
+
+
+
